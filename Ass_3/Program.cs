395687210d28ed50fe979196eebc292eda7f9af7
youssef fouad <yfo2sh@outlook.com>
2026-09-15 06:52:56 +0300

    1st answer

--- Ass_3/Program.cs
+++ Ass_3/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1st answer
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
+            #endregion
         }
     }
 }
